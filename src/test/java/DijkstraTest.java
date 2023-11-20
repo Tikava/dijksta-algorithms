1214475274ee6d38cea76d8a4859742cf72a49ef
Tikava <wavoleniu@gmail.com>
2023-11-21 04:17:31 +0600

    Add parameterizedTest && valueSource annotations

--- src/test/java/DijkstraTest.java
+++ src/test/java/DijkstraTest.java
@@ -1,31 +1,32 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-
 public class DijkstraTest {
 
-    @Test
-    public void testOriginalDijkstra() {
-        List<Node<Integer>> graph = GraphGenerator.generateRandomGraph(100000);
+    @ParameterizedTest
+    @ValueSource(ints = { 10, 100, 1000, 10000, 100000 })
+    public void testOriginalDijkstra(int graphSize) {
+        List<Node<Integer>> graph = GraphGenerator.generateRandomGraph(graphSize);
 
         Dijkstra<Integer> dijkstra = new Dijkstra<>();
         long startTime = System.currentTimeMillis();
         dijkstra.calculateShortestPath(graph.get(0));
         long endTime = System.currentTimeMillis();
 
-        System.out.println("Original Dijkstra Time: " + (endTime - startTime) + " ms");
+        System.out.println("Original Dijkstra Time for " + graphSize + " nodes: " + (endTime - startTime) + " ms");
     }
 
-    @Test
-    public void testImprovedDijkstra() {
-        List<Node<Integer>> graph = GraphGenerator.generateRandomGraph(1000000);
+    @ParameterizedTest
+    @ValueSource(ints = { 10, 100, 1000, 10000, 100000, 1000000 })
+    public void testImprovedDijkstra(int graphSize) {
+        List<Node<Integer>> graph = GraphGenerator.generateRandomGraph(graphSize);
 
         ImprovedDijkstra<Integer> improvedDijkstra = new ImprovedDijkstra<>();
         long startTime = System.currentTimeMillis();
         improvedDijkstra.calculateShortestPath(graph.get(0));
         long endTime = System.currentTimeMillis();
-
-        System.out.println("Improved Dijkstra Time: " + (endTime - startTime) + " ms");
+        System.out.println("Improved Dijkstra Time for " + graphSize + " nodes: " + (endTime - startTime) + " ms");
     }
 }
